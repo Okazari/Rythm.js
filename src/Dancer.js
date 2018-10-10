c9d0e184bf4b49a8d8b9be92d5adee6c261d9316
Mitch <mitchellpalermo@gmail.com>
2018-10-10 14:34:04 -0500

    Added the font-weight dance according to the guidlines in the contribution file.

--- src/Dancer.js
+++ src/Dancer.js
@@ -16,6 +16,7 @@ import fontSize, { reset as fontSizeReset } from './dances/font-size.js'
 import borderWidth, {
   reset as borderWidthReset,
 } from './dances/border-width.js'
+import fontWeight, { reset as fontWeightReset } from './dances/font-weight.js'
 
 class Dancer {
   constructor() {
@@ -36,6 +37,7 @@ class Dancer {
     this.registerDance('kern', kern, kernReset)
     this.registerDance('borderWidth', borderWidth, borderWidthReset)
     this.registerDance('fontSize', fontSize, fontSizeReset)
+    this.registerDance('fontWeight', fontWeight, fontWeightReset)
   }
 
   registerDance(type, dance, reset = () => {}) {
